e3e6160eb2a39ab259124ca5f7c05958c5d2f7be
Ngu "Nathan" Dang <48393950+NathanDang97@users.noreply.github.com>
2026-06-06 02:48:46 -0400

    Add: minimum-path-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    # bottom-up DP solution, O(m * n) time and space
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        dp = [[float('inf')] * (cols + 1) for _ in range(rows + 1)]
+        dp[rows - 1][cols] = 0
+        dp[rows][cols - 1] = 0
+
+        for r in range(rows - 1, -1, -1):
+            for c in range(cols - 1, -1, -1):
+                dp[r][c] = grid[r][c] + min(dp[r + 1][c], dp[r][c + 1])
+
+        return dp[0][0] 
