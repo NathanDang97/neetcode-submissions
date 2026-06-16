1f08b4afa7a507c3be5f47bd38514083fe075fb7
Ngu "Nathan" Dang <48393950+NathanDang97@users.noreply.github.com>
2026-06-16 02:25:11 -0400

    Add: find-peak-element - submission-3

---
+++ Data Structures & Algorithms/find-peak-element/submission-3.py
@@ -0,0 +1,7 @@
+class Solution:
+    # linear scan solution, O(n) time, O(1) space
+    def findPeakElement(self, nums: List[int]) -> int:
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                return i
+        return len(nums) - 1
