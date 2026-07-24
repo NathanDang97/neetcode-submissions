2edce327b1de088ba3cbad4a0e833cbc023de13c
Ngu "Nathan" Dang <48393950+NathanDang97@users.noreply.github.com>
2026-07-24 02:28:45 -0400

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    // use two pointers to remove elements in place without extra space
+    // O(n) time, O(1) space
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
