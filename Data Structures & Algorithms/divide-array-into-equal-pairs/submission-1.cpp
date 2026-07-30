d5b14c95f9c8637d888367014f03e75a3038076d
Ngu "Nathan" Dang <48393950+NathanDang97@users.noreply.github.com>
2026-07-29 23:30:26 -0400

    Add: divide-array-into-equal-pairs - submission-1

---
+++ Data Structures & Algorithms/divide-array-into-equal-pairs/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // frequency counter solution, O(n) time and space
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> counter;
+        for (int num : nums) {
+            counter[num]++;
+        }
+
+        for (auto& [key, count] : counter) {
+            if (count % 2 != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
